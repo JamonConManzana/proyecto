dd27a94fb098698e2310e5d83e0a2baa8ea9b3d4
Javier Monedero <jmanzano099@gmail.com>
2025-07-03 15:00:23 -0500

    eeee

--- principal.py
+++ principal.py
@@ -8,7 +8,7 @@ app.secret_key = 'your_secret_key'
 app.config['CARPETAU'] = 'uploads'
 
 # MySQL configuration
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'localhost' 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'dbtrastearte'  
